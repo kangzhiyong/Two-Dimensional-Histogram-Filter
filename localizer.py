45c16495cc8f815da92c0fd196ad820d0b9e7dc3
kangzhiyong <850551720@qq.com>
2019-10-16 22:41:13 +0800

    Add File

---
+++ localizer.py
@@ -0,0 +1,49 @@
+#import pdb
+from helpers import normalize, blur
+
+def initialize_beliefs(grid):
+    height = len(grid)
+    width = len(grid[0])
+    area = height * width
+    belief_per_cell = 1.0 / area
+    beliefs = []
+    for i in range(height):
+        row = []
+        for j in range(width):
+            row.append(belief_per_cell)
+        beliefs.append(row)
+    return beliefs
+
+def sense(color, grid, beliefs, p_hit, p_miss):
+    new_beliefs = []
+
+    #
+    # TODO - implement this in part 2
+    #
+    height = len(grid)
+    width = len(grid[0])
+    for i in range(height):
+        row = []
+        for j in range(width):
+            isColor = (grid[i][j] == color);
+            row.append(beliefs[i][j] * p_hit * isColor + beliefs[i][j] * p_miss * (1 - isColor))
+        new_beliefs.append(row)
+    s = 0
+    for i in range(height):
+        s += sum(new_beliefs[i])
+    for i in range(height):
+        for j in range(width):
+            new_beliefs[i][j] /= s
+    return new_beliefs
+
+def move(dy, dx, beliefs, blurring):
+    height = len(beliefs)
+    width = len(beliefs[0])
+    new_G = [[0.0 for i in range(width)] for j in range(height)]
+    for i, row in enumerate(beliefs):
+        for j, cell in enumerate(row):
+            new_i = (i + dy) % width
+            new_j = (j + dx) % height
+            #pdb.set_trace()
+            new_G[int(new_j)][int(new_i)] = cell
+    return blur(new_G, blurring)
